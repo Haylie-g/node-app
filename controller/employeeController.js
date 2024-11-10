9131e881d15a7dd602d8e6bc29824a2845aecdc0
Haylie Guzman <hguzma15@stu.southtexascollege.edu>
2024-11-10 16:30:41 -0600

    Delete is working but commented out

--- controller/employeeController.js
+++ controller/employeeController.js
@@ -29,3 +29,38 @@ export const fetch = async (req, res) => {
         res.status(500).json({ error: "Internal Server Error" });
     }
 }
+
+export const update = async(req, res) => {
+    try {
+        const id = req.params.id
+        const employeeExist = await Employee.findOne({_id:id})
+
+        if(!employeeExist){
+            return res.status(404).json({message: "Employee not found"})
+        }
+
+        const updateEmployee = await Employee.findByIdAndUpdate(id, req.body, {new: true})
+        res.status(201).json(updateEmployee)
+    
+    } catch (error){
+        res.status(500).json({error: "Internal Server Error"})
+    }
+}
+
+//export const deleteEmployee = async(req, res) => {
+//   try{
+//        const id = req.params.id
+//        const employeeExist = await Employee.findOne({_id:id})
+//
+//        if(!employeeExist) {
+//            return res.status(404).json({message: "Employee not found"})
+//        }
+//
+//        await Employee.findByIdAndDelete(id)
+//        res.status(201).json({message: "Employee terminate"})
+//    }catch (error){
+//        res.status(500).json({error: "Internal Server Error"})
+//    }
+//}
+
+
